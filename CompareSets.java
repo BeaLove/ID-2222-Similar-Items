66c4452871c02b5ce10df1bd4f09577fe9f61b7b
BeaLove <beatriceelovely@gmail.com>
2020-11-02 15:36:02 +0100

    CompareSets beräknar jaccard similarity

---
+++ CompareSets.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+
+public class CompareSets {
+        HashSet<Integer> set1 = new HashSet<Integer>();
+        HashSet<Integer> set2 = new HashSet<Integer>();
+    public CompareSets(HashSet<Integer> set_1, HashSet<Integer> set_2) {
+        set1 = set_1;
+        set2 = set_2;
+    }
+   
+    public float jaccardSimilarity() {
+        HashSet<Integer> union = new HashSet<Integer>();
+        HashSet<Integer> intersection = new HashSet<Integer>();
+        union.addAll(set1);
+        union.addAll(set2);
+        intersection.retainAll(set1);
+        intersection.retainAll(set2);
+
+        int union_size = union.size();
+        int intersection_size = intersection.size();
+
+        float similarity = union_size/intersection_size;
+
+        return similarity;
+
+
+    }
+}
