0a9404d6a9604654919a11895da3064caf5dabc6
BeaLove <beatriceelovely@gmail.com>
2020-11-04 13:42:07 +0100

    lite debuggat

--- MinHash.java
+++ MinHash.java
@@ -1,5 +1,9 @@
 import java.util.HashSet;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 /**Random r = new Random(11);
 for (int i = 0; i < numHash; i++){
@@ -16,19 +20,29 @@ public class MinHash {
    //IN: number of hash functions, list of all documents as sets of hashed shingles
    //OUT: n x s signature matrix where each column s is the MinHash signature of a document,
    int s;
-   Array<HashSet<Integer>> docs = new List<HashSet<Integer>>(); 
-    public void MinHash(int hashfunc, Array<HashSet<Integer>> all_docs) {
+   List<HashSet<Integer>> docs = new List<HashSet<Integer>>(); 
+    public void MinHash(int n_hash, List<HashSet<Integer>> all_docs) {
         docs = all_docs;
         s = docs.size();
-        int[][] signatures = int[hashfunc][s];
+        int[][] signatures = new int[n_hash][s];
         Integer inf = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n_hash; i++){
             for (int j = 0; j < s; j++){
                 signatures[i][j] = inf;
             }
         }
     }
+    public int[][] hashConstants(int n_funcs) {
+        int[][] hash_constants = new int[n_funcs][3];
+        int a;
+        int b;
+        int c = 1009; //nearest prime number above max value of number to be hashed 
+        var rand = new Random();
+        for (int n = 0; n < n_funcs; n++) {
+            a = rand.nextInt(int)
+        }
 
+    }
     public HashSet<Integer> makeMasterSet() {
         HashSet<Integer> master_set = new HashSet<Integer>();
         //loopa genom alla dokument och skapa en master list med shingles
@@ -39,7 +53,7 @@ public class MinHash {
     }
 
     public void minHash(HashSet<Integer> master_set, List hashfunc) {
-        int num_hash = hashfunc.size(); \\number of hash functions
+        int num_hash = hashfunc.size(); //number of hash functions
         int shingles = master_set.size();
         int num_docs = docs.size();
         for (int i = 0; i = num_docs; i++){
